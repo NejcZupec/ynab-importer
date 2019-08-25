49d06fdd76b87af46759e92dd66754f339532e83
Nejc Zupec <zupec.nejc@gmail.com>
2019-08-25 13:06:38 +0200

    Don't log to file ynab_importer.log

--- lib/log.py
+++ lib/log.py
@@ -5,7 +5,6 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)-8s %(message)s',
     handlers=[
-        logging.FileHandler('ynab_importer.log'),
         logging.StreamHandler(),
     ],
 )
