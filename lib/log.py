3ee844428b825502e3914572a56f4171dbb3a39c
Nejc Zupec <zupec.nejc@gmail.com>
2019-08-25 14:36:31 +0200

    Enable Python logging in AWS Lambda

--- lib/log.py
+++ lib/log.py
@@ -1,6 +1,13 @@
 import logging
 
 
+# this hack is needed to make AWS Lambda logs working
+# https://stackoverflow.com/questions/1943747
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)-8s %(message)s',
