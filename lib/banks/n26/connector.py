afe5acc568f4e768daff77dcb74474df7efa920d
Nejc Zupec <zupec.nejc@gmail.com>
2019-09-30 09:31:19 +0200

    Upgrade n26 library to get 2FA implementation

--- lib/banks/n26/connector.py
+++ lib/banks/n26/connector.py
@@ -8,6 +8,7 @@ class N26Connector(object):
         self._api = api.Api(config.Config(
             username=username,
             password=password,
+            login_data_store_path=f".n26_access_token-{username}",
         ))
 
     def get_transactions(self, limit=50):
